9cd103cf2a4c3f5d45abd7758cf60eb18cb5ef81
Nahuel <nahuelavila670@gmail.com>
2023-04-25 13:02:52 -0300

    Fix warnings
    - Make some changes on img tags
    - Replacing ' with "

--- src/components/WAICard.jsx
+++ src/components/WAICard.jsx
@@ -1,8 +1,12 @@
+import Image from 'next/image'
+
 export function WAICard() {
     return(
-        <div className='wai-card-container p-12 w-full h-full grid place-content-center h-screen min-h-[30rem]'>
+        <div className="wai-card-container p-12 w-full h-full grid place-content-center h-screen min-h-[30rem]">
             <article className="grid place-content-center w-64 text-center gap-6">
-                <img className="wai-card-img w-full shadow-xl rounded-full" src="https://img-9gag-fun.9cache.com/photo/aXvbpZb_460s.jpg"></img>
+                <picture>
+                    <img className="wai-card-img w-full shadow-xl rounded-full" alt='profile photo' src="https://media.licdn.com/dms/image/C4E03AQE0I-L1hA4B6g/profile-displayphoto-shrink_400_400/0/1619910838000?e=1687996800&v=beta&t=B4zZv9lmPjajgKKXiIyx_lLshzWOLAqg1gPkEfcdkoE" />
+                </picture>
                 <div className="horizontal-line bg-white/10 h-0.5 "></div>
                 <p className="wai-card-title font-roboto font-bold leading-[3rem]">Hello,</p>
                 <p className="wai-card-subtitle font-roboto font-light leading-[1rem]">I am Nahuel.</p>
