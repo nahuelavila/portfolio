7b5718c876345a2cd29e5046d8010f939a716966
Nahuel <nahuelavila670@gmail.com>
2023-04-25 15:18:23 -0300

    Delete Skill.jsx

--- src/components/Skill.jsx
+++
@@ -1,7 +0,0 @@
-export function Skill({ name, level }) {
-    return(
-        <section className="container rounded-md border-4 border-sky-300 mb-2">
-            <p className="">{name} - {level}</p>
-        </section>
-    )
-}
