ed023f06d31f2b5d77b63fd2d267e040bb73990c
Nahuel <nahuelavila670@gmail.com>
2023-05-09 11:12:59 -0300

    Add title and meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import { WAICard } from "../components/WAICard.jsx";
 import { WIDCard } from "../components/WIDCard.jsx";
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Nahuel Avila</title>
+        <meta name="description" content="Nahuel Avila's portfolio" />
+        <meta name="viewport" content="width=device-width"/>
+      </Head>
       {/* <section className="header">header</section> */}
       <section className="whoamai-card">
         <WAICard />
