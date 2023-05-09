775e407755b956fb31da0bfd00b3916920c8fd0e
Nahuel <nahuelavila670@gmail.com>
2023-05-09 11:00:15 -0300

    Make WIDCard more readable on mobile

--- src/components/WIDCard.jsx
+++ src/components/WIDCard.jsx
@@ -5,9 +5,9 @@ export function WIDCard() {
         <div className="whatido wid-card-container p-12 grid place-content-center">
             <article className="grid place-content-center text-center gap-6">
                 <div className="horizontal-line bg-white/10 h-0.5 "></div>
-                <p className="wid-card-title font-bold leading-[3rem]">What can I offer you?</p>
+                <p className="wid-card-title font-bold text-[3rem] leading-[2.5rem] sm:text-[3.8rem] sm:leading-[3rem]">What can I offer you?</p>
                 <div className="wid-skills">
-                    <p className="wid-card-subtitle font-light leading-[1rem]">
+                    <p className="wid-card-subtitle font-light sm:leading-[1rem]">
                         I&apos;m skilled on a variety of technologies such as
                         <Image className="inline ml-1.5" alt="python" width='22' height='22' src="/python.svg" />
                         <Image className="inline ml-1.5" alt="javascript" width='22' height='22' src="/js.svg" />
