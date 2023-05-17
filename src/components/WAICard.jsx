f8119b70d4b604907baa285c26c2659e6ec56bb1
Nahuel <nahuelavila670@gmail.com>
2023-05-17 15:52:48 -0300

    Add linkedin ref img

--- src/components/WAICard.jsx
+++ src/components/WAICard.jsx
@@ -7,13 +7,16 @@ export function WAICard() {
                 <picture>
                     <img className="wai-card-img w-full shadow-xl rounded-full" alt='profile photo' src="/me.png" />
                 </picture>
+                <div className="linkedin-ref grid place-content-center">
+                    <a className="grid place-content-center" href='https://www.linkedin.com/in/nahuel-avila/'><Image src="/linkedin.svg" alt="LinkedIn" width='24' height='24' /></a>    
+                </div>
                 <div className="horizontal-line bg-white/10 h-0.5 "></div>
                 <h1 className="wai-card-title font-roboto font-bold leading-[3rem]">Hello,</h1>
                 <p className="wai-card-subtitle font-roboto font-light leading-[1rem]">I am Nahuel.</p>
                 <p className="wai-card-subtitle font-roboto font-light leading-[1rem]">Let me know how can I help you,</p>
                 <a href="mailto:nahuel@nahuelavila.com" className="contact-me text-white border rounded-full p-1 hover:bg-white hover:text-black">let&apos;s chat!</a>
             </article>
-            <div className="go-down relative grid place-content-center top-16 sm:top-24">
+            <div className="go-down relative grid place-content-center top-12 sm:top-20">
                 <Image className="go-down-img animate-bounce" alt="go down btn" src="/arrow-down-solid.svg" height='24' width='24' />
             </div>
         </div>
