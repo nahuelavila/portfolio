d8bc03b848002cb0e56c3525cb8348e9b65e213e
Nahuel <nahuelavila670@gmail.com>
2026-03-10 20:07:19 -0300

    Rename div ID in WIDCard component for consistency

--- src/components/WIDCard.jsx
+++ src/components/WIDCard.jsx
@@ -4,7 +4,7 @@ import { Test } from './ArchivoTest'
 
 export function WIDCard() {
     return(
-        <div id="whatidoa" className="whatido wid-card-container p-4 sm:p-8 md:p-12 grid place-content-center bg-[#ffffff]">
+        <div id="whatidoss" className="whatido wid-card-container p-4 sm:p-8 md:p-12 grid place-content-center bg-[#ffffff]">
             <h1 className="grid place-items-center wid-card-title font-bold text-2xl sm:text-3xl md:text-[3.8rem] leading-[2rem] sm:leading-[2.5rem] md:leading-[3rem] text-gray-800">What about me?</h1>
             <div className="horizontal-line bg-black/10 h-0.5 my-4 sm:my-5"></div>
             <article className="grid grid-cols-1 place-items-center text-center gap-6 md:gap-8 max-w-3xl mx-auto">
