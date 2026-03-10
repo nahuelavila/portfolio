e01ea747b47f5cc8d22e9c30302c333255524eae
Nahuel <nahuelavila670@gmail.com>
2026-03-10 20:10:39 -0300

    Add import for Test3 component in WIDCard.jsx

--- src/components/WIDCard.jsx
+++ src/components/WIDCard.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { SkillsSection } from './SkillsSection'
 import { Test } from './ArchivoTest'
 import { Test2 } from './ArchivoTestDos'
+import { Test3 } from './ArchivoTestDos'
 
 export function WIDCard() {
     return(
